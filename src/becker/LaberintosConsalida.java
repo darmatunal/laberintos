072b09e6fe699858f8daf13ce97b97a082285ecf
darmatunal <dmarangoa@unal.edu.co>
2017-03-17 23:58:54 -0500

    Add files via upload

---
+++ src/becker/LaberintosConsalida.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laberintosconsalida;
+import becker.robots.*;
+
+ *
+ * @author Dario Arango
+ */
+public class LaberintosConsalida {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
